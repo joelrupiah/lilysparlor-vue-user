ab6ce1a1cfb78a5def7f63be7858e6e79085c7ee
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-11-25 06:21:23 +0000

    cart

--- src/components/inc/UserHeader.vue
+++ src/components/inc/UserHeader.vue
@@ -1,139 +1,151 @@
 <template>
-  <div id="user-header">
-    <div class="header-top">
-      <div class="container">
-        <div class="header-left">
-          <a href="tel:#"><i class="icon-phone"></i>Call: +0123 456 789</a>
-        </div><!-- End .header-left -->
+<div id="user-header">
+  <div class="header-top">
+    <div class="container">
+      <div class="header-left">
+        <a href="tel:#"><i class="icon-phone"></i>Call: +0123 456 789</a>
+      </div><!-- End .header-left -->
 
-        <div class="header-right">
+      <div class="header-right">
 
-          <ul class="top-menu">
-            <li>
-              <a href="#">Links</a>
-              <ul>
-                <!-- <li><a href="#signin-modal" data-toggle="modal">Sign in / Sign up</a></li> -->
-                <li><a href="/auth">Sign in / Sign up</a></li>
-              </ul>
-            </li>
-          </ul><!-- End .top-menu -->
-        </div><!-- End .header-right -->
+        <ul class="top-menu">
+          <li>
+            <a href="#">Links</a>
+            <ul>
+              <!-- <li><a href="#signin-modal" data-toggle="modal">Sign in / Sign up</a></li> -->
+              <li><a href="/auth">Sign in / Sign up</a></li>
+            </ul>
+          </li>
+        </ul><!-- End .top-menu -->
+      </div><!-- End .header-right -->
 
-      </div><!-- End .container -->
-    </div><!-- End .header-top -->
+    </div><!-- End .container -->
+  </div><!-- End .header-top -->
 
-    <div class="header-middle">
-      <div class="container">
-        <div class="header-left">
-          <button class="mobile-menu-toggler">
-            <span class="sr-only">Toggle mobile menu</span>
-            <i class="icon-bars"></i>
-          </button>
+  <div class="header-middle">
+    <div class="container">
+      <div class="header-left">
+        <button class="mobile-menu-toggler">
+          <span class="sr-only">Toggle mobile menu</span>
+          <i class="icon-bars"></i>
+        </button>
 
-          <a href="/" class="logo">
-            <img src="assets/images/demos/demo-4/logo.png" alt="Molla Logo" width="105" height="25">
+        <a href="/" class="logo">
+          <img src="assets/images/demos/demo-4/logo.png" alt="Molla Logo" width="105" height="25">
+        </a>
+      </div><!-- End .header-left -->
+
+      <div class="header-center">
+        <div class="header-search header-search-extended header-search-visible d-none d-lg-block">
+          <a href="#" class="search-toggle" role="button"><i class="icon-search"></i></a>
+          <form action="#" method="get">
+            <div class="header-search-wrapper search-wrapper-wide">
+              <label for="q" class="sr-only">Search</label>
+              <button class="btn btn-primary" type="submit"><i class="icon-search"></i></button>
+              <input type="search" class="form-control" name="q" id="q" placeholder="Search product ..." required>
+            </div><!-- End .header-search-wrapper -->
+          </form>
+        </div><!-- End .header-search -->
+      </div>
+
+      <div class="header-right">
+        <div class="wishlist">
+          <a href="/wishlist" title="Wishlist">
+            <div class="icon">
+              <i class="icon-heart-o"></i>
+              <span class="wishlist-count badge"></span>
+            </div>
+            <p>Wishlist</p>
           </a>
-        </div><!-- End .header-left -->
+        </div><!-- End .compare-dropdown -->
 
-        <div class="header-center">
-          <div class="header-search header-search-extended header-search-visible d-none d-lg-block">
-            <a href="#" class="search-toggle" role="button"><i class="icon-search"></i></a>
-            <form action="#" method="get">
-              <div class="header-search-wrapper search-wrapper-wide">
-                <label for="q" class="sr-only">Search</label>
-                <button class="btn btn-primary" type="submit"><i class="icon-search"></i></button>
-                <input type="search" class="form-control" name="q" id="q" placeholder="Search product ..." required>
-              </div><!-- End .header-search-wrapper -->
-            </form>
-          </div><!-- End .header-search -->
-        </div>
+        <div class="dropdown cart-dropdown">
+          <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
+            <div class="icon">
+              <i class="icon-shopping-cart"></i>
+              <span class="cart-count">{{ cartCount }}</span>
+            </div>
+            <p>Cart</p>
+          </a>
 
-        <div class="header-right">
-          <div class="wishlist">
-            <a href="/wishlist" title="Wishlist">
-              <div class="icon">
-                <i class="icon-heart-o"></i>
-                <span class="wishlist-count badge">3</span>
-              </div>
-              <p>Wishlist</p>
-            </a>
-          </div><!-- End .compare-dropdown -->
+          <div class="dropdown-menu dropdown-menu-right">
+            <div class="dropdown-cart-products">
+              <div class="product">
+                <div class="product-cart-details">
+                  <h4 class="product-title">
+                    <a href="product.html">{{item.product.name}}</a>
+                  </h4>
 
-          <div class="dropdown cart-dropdown">
-            <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
-              <div class="icon">
-                <i class="icon-shopping-cart"></i>
-                <span class="cart-count">2</span>
-              </div>
-              <p>Cart</p>
-            </a>
+                  <span class="cart-product-info">
+                    <span class="cart-product-qty">{{item.quantity}}</span>
+                    x ${{item.product.price}}
+                  </span>
+                </div><!-- End .product-cart-details -->
 
-            <div class="dropdown-menu dropdown-menu-right">
-              <div class="dropdown-cart-products">
-                <div class="product">
-                  <div class="product-cart-details">
-                    <h4 class="product-title">
-                      <a href="product.html">Beige knitted elastic runner shoes</a>
-                    </h4>
+                <figure class="product-image-container">
+                  <a href="product.html" class="product-image">
+                    <img src="assets/images/products/cart/product-1.jpg" alt="product">
+                  </a>
+                </figure>
+                <a href="#" class="btn-remove" title="Remove Product" @click.prevent="removeProductInCart(item.product)">
+                  <i class="icon-close"></i>
+                </a>
+              </div><!-- End .product -->
 
-                    <span class="cart-product-info">
-                      <span class="cart-product-qty">1</span>
-                      x $84.00
-                    </span>
-                  </div><!-- End .product-cart-details -->
+            </div><!-- End .cart-product -->
 
-                  <figure class="product-image-container">
-                    <a href="product.html" class="product-image">
-                      <img src="assets/images/products/cart/product-1.jpg" alt="product">
-                    </a>
-                  </figure>
-                  <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
-                </div><!-- End .product -->
+            <div class="dropdown-cart-products" v-if="emptyCart()">
+              <h6 class="text-center text-danger">Cart is empty</h6>
+            </div><!-- End .cart-product -->
 
-                <div class="product">
-                  <div class="product-cart-details">
-                    <h4 class="product-title">
-                      <a href="product.html">Blue utility pinafore denim dress</a>
-                    </h4>
+            <div class="dropdown-cart-total">
+              <span>Total</span>
 
-                    <span class="cart-product-info">
-                      <span class="cart-product-qty">1</span>
-                      x $76.00
-                    </span>
-                  </div><!-- End .product-cart-details -->
+              <span class="cart-total-price">${{totalCartPrice}}</span>
+            </div><!-- End .dropdown-cart-total -->
 
-                  <figure class="product-image-container">
-                    <a href="product.html" class="product-image">
-                      <img src="assets/images/products/cart/product-2.jpg" alt="product">
-                    </a>
-                  </figure>
-                  <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
-                </div><!-- End .product -->
-              </div><!-- End .cart-product -->
-
-              <div class="dropdown-cart-total">
-                <span>Total</span>
-
-                <span class="cart-total-price">$160.00</span>
-              </div><!-- End .dropdown-cart-total -->
-
-              <div class="dropdown-cart-action">
-                <a href="/cart" class="btn btn-primary">View Cart</a>
-                <a href="/checkout" class="btn btn-outline-primary-2"><span>Checkout</span><i class="icon-long-arrow-right"></i></a>
-              </div><!-- End .dropdown-cart-total -->
-            </div><!-- End .dropdown-menu -->
-          </div><!-- End .cart-dropdown -->
-        </div><!-- End .header-right -->
-      </div><!-- End .container -->
-    </div><!-- End .header-middle -->
-  </div>
+            <div class="dropdown-cart-action">
+              <a href="/cart" class="btn btn-primary">View Cart</a>
+              <a href="/checkout" class="btn btn-outline-primary-2"><span>Checkout</span><i class="icon-long-arrow-right"></i></a>
+            </div><!-- End .dropdown-cart-total -->
+          </div><!-- End .dropdown-menu -->
+        </div><!-- End .cart-dropdown -->
+      </div><!-- End .header-right -->
+    </div><!-- End .container -->
+  </div><!-- End .header-middle -->
+</div>
 </template>
 
 <script>
 export default {
   name: 'UserHeader',
-  computed(){
-    
+  data() {
+    return {}
+  },
+  methods: {
+    async getStoredCartItems() {
+      await this.$store.dispatch('cart/getStoredCartItems')
+    },
+    removeProductInCart(product) {
+      this.$store.dispatch('cart/removeProductInCart', product)
+    },
+    emptyCart() {
+      return (this.cart.length < 1)
+    },
+  },
+  computed: {
+    cartCount() {
+      return this.$store.getters['cart/cartCountItem']
+    },
+    cart() {
+      return this.$store.getters['cart/getCartItem']
+    },
+    totalCartPrice() {
+      return this.$store.getters['cart/cartTotalPrice']
+    }
+  },
+  async mounted() {
+    await this.getStoredCartItems()
   }
 }
 </script>
